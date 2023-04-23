934b5484f816662b751e779bf51c8452e845ffa7
IvanProkhorets <prohorets.ivan@gmail.com>
2023-04-23 20:47:45 +0300

    fix toggle

--- js/toggle-menu.js
+++ js/toggle-menu.js
@@ -1,5 +1,4 @@
 function toggleMenu() {
   document.getElementsByClassName('header')[0].classList.toggle('close-menu');
-  // document.getElementsByClassName('burger-btn__open')[0].classList.toggle('hidden');
   document.getElementsByClassName('burger-btn__close')[0].classList.toggle('hidden');
 }
