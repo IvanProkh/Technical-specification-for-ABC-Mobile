dbc3bc54160f894886f144d3d774e4e5e4d96e9e
IvanProkhorets <prohorets.ivan@gmail.com>
2023-04-23 23:29:02 +0300

    add toggleDropdown

--- js/toggle-menu.js
+++ js/toggle-menu.js
@@ -2,3 +2,8 @@ function toggleMenu() {
   document.getElementsByClassName('header')[0].classList.toggle('close-menu');
   document.getElementsByClassName('burger-btn__close')[0].classList.toggle('hidden');
 }
+
+function toggleDropdown() {
+  document.getElementsByClassName('hero__button-dropdown')[0].classList.toggle('active');
+  document.getElementsByClassName('info')[0].classList.toggle('close-info');
+}
